4a8027e76d8233f4069457cc34e146334ed5b5fe
Chris Southwick <csouthwick@live.com>
2020-09-19 14:26:31 -0400

    add displayMovies function

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,82 +4,97 @@ var actorInput = document.querySelector("#actor-input")
 
 
 
-var getPersonId = function(actor) {
+var getPersonId = function (actor) {
 
 
-    var apiUrl =
-        "https://api.themoviedb.org/3/search/person?api_key=0bd9398a9daad70a50c685a4f8c0a74b&language=en-US&query=" + encodeURIComponent(actor) + "&page=1&include_adult=false"
+  var apiUrl =
+    "https://api.themoviedb.org/3/search/person?api_key=0bd9398a9daad70a50c685a4f8c0a74b&language=en-US&query=" + encodeURIComponent(actor) + "&page=1&include_adult=false"
 
 
-    fetch(apiUrl).then(function(response) {
-        response.json().then(function(data) {
-            var searchOption = {
-                actor: data.results[0].id,
-                genre: $("#genre-dropdown").val().trim()
+  fetch(apiUrl).then(function (response) {
+    response.json().then(function (data) {
+      var searchOption = {
+        actor: data.results[0].id,
+        genre: $("#genre-dropdown").val().trim()
 
-            }
-            getMovies(searchOption)
+      }
+      getMovies(searchOption)
 
-        })
+    })
 
-    });
+  });
 
 
 
 
 };
 
-var searchHandler = function(event) {
-    var searchGenre = document.querySelector("#genre-dropdown")
-    var actorInput = document.querySelector("#actor-input")
+var searchHandler = function (event) {
+  var searchGenre = document.querySelector("#genre-dropdown")
+  var actorInput = document.querySelector("#actor-input")
 
-    var actor = actorInput.value.trim();
+  var actor = actorInput.value.trim();
 
-    if (!actor) {
+  if (!actor) {
 
-        $(".modal-card-title").text("Please Enter Actor or Actress")
-        $(".modal").addClass("is-active is-clipped")
-    }
+    $(".modal-card-title").text("Please Enter Actor or Actress")
+    $(".modal").addClass("is-active is-clipped")
+  }
 
-    $("#close-modal-btn").click(function() {
-        $(".modal").removeClass("is-active");
-    });
+  $("#close-modal-btn").click(function () {
+    $(".modal").removeClass("is-active");
+  });
 
-    getPersonId(actor)
+  getPersonId(actor)
 }
 
 
 
 // options should be an object with genre and actor properties
 function getMovies(options) {
-    var apiUrl = "https://api.themoviedb.org/3/discover/movie?api_key=0bd9398a9daad70a50c685a4f8c0a74b&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1";
+  var apiUrl = "https://api.themoviedb.org/3/discover/movie?api_key=0bd9398a9daad70a50c685a4f8c0a74b&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1";
 
-    // add the desired genre and actor to the query string
-    apiUrl += "&with_genres=" + options.genre;
-    apiUrl += "&with_cast=" + options.actor;
+  // add the desired genre and actor to the query string
+  // hardcoded genre for now
+  apiUrl += "&with_genres=" + "27"; //options.genre;
+  apiUrl += "&with_cast=" + options.actor;
 
-    // fetch from the API
-    fetch(apiUrl).then(function(response) {
-        if (response.ok) {
-            response.json().then(function(data) {
-                // data.results will be an array of movie info
-                displayMovies(data.results);
+  // fetch from the API
+  fetch(apiUrl).then(function (response) {
+    if (response.ok) {
+      response.json().then(function (data) {
+        // data.results will be an array of movie info
+        displayMovies(data.results);
 
-            });
-        } else {
-            displayError("Error: " + response.statusText);
-        }
+      });
+    } else {
+      displayError("Error: " + response.statusText);
+    }
 
-    }).catch(function(error) {
-        displayError("Unable to connect to TMDb");
-    });
+  }).catch(function (error) {
+    displayError("Unable to connect to TMDb");
+  });
 }
 
+
+function displayMovies(movieData) {
+  var movieListEl = document.getElementById("now-playing");
+  var tempHtml = "";
+
+  for (var i = 0; i < movieData.length; i++) {
+    tempHtml += "<li class='is-size-4' data-movie-id='" + movieData[i].id + "'>" + movieData[i].title;
+    tempHtml += " <span class='icon has-text-info is-medium'><i class='fas fa-lg fa-plus-square'></i></span></li>";
+  }
+
+  movieListEl.innerHTML = tempHtml;
+}
+
+
 popcornBtn.addEventListener("click", searchHandler)
-    // this allows the user to search by using the enter button instead of just the search button click event
-actorInput.addEventListener("keyup", function(event) {
-    // Number 13 is the "Enter" key on the keyboard
-    if (event.keyCode === 13) {
-        searchHandler(event)
-    }
+// this allows the user to search by using the enter button instead of just the search button click event
+actorInput.addEventListener("keyup", function (event) {
+  // Number 13 is the "Enter" key on the keyboard
+  if (event.keyCode === 13) {
+    searchHandler(event)
+  }
 })
